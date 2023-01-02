665f52707fff1a347c01a17b6e7c319f5864d4b4
ArtiCOD <bulgakov97@mail.ru>
2023-01-02 11:41:47 +0300

    homework

--- src/store/messages/selectors.js
+++
@@ -1,9 +0,0 @@
-import { nanoid } from 'nanoid'
-
-export const selectChat = (state) => Object.keys(state.messages).map((chat) => ({
-  id: nanoid(),
-  name: chat
-}))
-
-
-export const selectMessage = (state) => state.messages
--- src/utils/ThemeContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const defaultContext = {
-  theme: 'light'
-}
-
-export const ThemeContext = React.createContext(defaultContext)
