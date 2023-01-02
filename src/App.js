a86270469c714cbe46938d0214119d1e7594aa60
ArtiCOD <bulgakov97@mail.ru>
2023-01-02 11:16:54 +0300

    homework7

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
+import { Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import { useState } from 'react'
-import { Form } from './components/classes/Form'
-import { Form as FormFunc } from './components/func/Form'
+import { PersistGate } from 'redux-persist/integration/react'
+import { nanoid } from 'nanoid'
+
+import { defaultContext, ThemeContext } from './utils/ThemeContext'
+import { store, persistor } from './store'
+
+import { Header } from './components/Header/Header'
+import { MainPage } from './pages/MainPage'
+import { ProfilePage } from './pages/ProfilePage'
+import { AboutWithConnect } from './pages/AboutPage'
+import { ChatsPage } from './pages/ChatsPage/ChatsPage'
+import { ChatList } from './components/ChatList/ChatList'
 
 export function App () {
-  const [toggle, setToggle] = useState(true)
-  const [arr, setArr] = useState([{ name: 'Biba' }, { name: 'Boba' }, { name: 'Giga' }, { name: 'Goga' }])
+  const [theme, setTheme] = useState(defaultContext.theme)
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
+
   return (
     <>
-      <Form />
-      <hr />
-      <button onClick={() => setToggle(!toggle)}>{toggle ? 'hide' : 'show'}</button>
-      {toggle && <FormFunc title='Function Component!!!!!!' />}
-      <ul>
-        {arr.map((item) => (
-          <li>{item.name}</li>
-        ))}
-      </ul>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <ThemeContext.Provider value={{
+            theme,
+            toggleTheme
+          }}>
+            <Routes>
+              <Route path='/' element={<Header />}>
+                <Route index element={<MainPage />} />
+                <Route path="profile" element={<ProfilePage />} />
+                <Route path="about" element={<AboutWithConnect />} />
+                <Route path="chats">
+                  <Route index element={<ChatList />} />
+                  <Route
+                    path=":chatId"
+                    element={<ChatsPage />}
+                  />
+                </Route>
+              </Route>
+
+              <Route path="*" element={<h2>404 Page not FOUND</h2>} />
+            </Routes>
+          </ThemeContext.Provider>
+        </PersistGate>
+      </Provider>
     </>
   )
 }
-
-// export default App
